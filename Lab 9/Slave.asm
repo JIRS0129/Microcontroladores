f2a052228a7a37bd41d1b1c630e03ce3cca20646
Jorge Lorenzana <lor17302@uvg.edu.gt>
2018-10-31 12:32:55 -0600

    Codigo de Slave modificado para nuestras necesidades

--- Lab 9/Slave.asm
+++ Lab 9/Slave.asm
@@ -1,12 +1,11 @@
-;*******************************************************************************
+;***************************
 ;                                                                              *
-;    Filename:    Serial.asm
-;    Autor: José Eduardo Morales					
-;    Description: EJEMPLO de serial y ADC                                      *
-;   El código convierte un valor del adc y lo guarda en el puerto b. A la vez
-;   lo envía a través del TX. También recibe un dato y este lo muestra en el 
-;   puerto d. Para ver funcionar ambos se puede colocar un jumper entre rx y tx
-;*******************************************************************************
+;    Filename: Slave.asm
+;    Autor: Jorge Lorenzana					
+;    Description: Codigo de Slave para Lab9                                     *
+;   El código recibe un dato y este lo muestra en el 
+;   puerto a.
+;***************************
 #include "p16f887.inc"
 
 ; CONFIG1
@@ -15,79 +14,58 @@
 ; CONFIG2
 ; __config 0xFFFF
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
-;*******************************************************************************
+;***************************
    GPR_VAR        UDATA
-   W_TEMP         RES        1      ; w register for context saving (ACCESS)
-   STATUS_TEMP    RES        1      ; status used for context saving
    DELAY1	  RES	    1
    DELAY2	  RES	    1
-;*******************************************************************************
+;***************************
 ; Reset Vector
-;*******************************************************************************
+;***************************
 
 RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
 
-;*******************************************************************************
-;ISR       CODE    0x0004           ; interrupt vector location
-;     RETFIE
-;*******************************************************************************
+;***************************
 ; MAIN PROGRAM
-;*******************************************************************************
+;***************************
 
 MAIN_PROG CODE                      ; let linker place main program
 
 START
-;*******************************************************************************
+;***************************
     CALL    CONFIG_RELOJ		; RELOJ INTERNO DE 500KHz
     CALL    CONFIG_IO
     CALL    CONFIG_TX_RX		; 10417hz
     CALL    CONFIG_ADC			; canal 0, fosc/8, adc on, justificado a la izquierda, Vref interno (0-5V)
     BANKSEL PORTA
-;*******************************************************************************
+;***************************
    
-;*******************************************************************************
+;***************************
 ; CICLO INFINITO
-;*******************************************************************************
+;***************************
 LOOP:
     CALL    DELAY_50MS
-    BSF	    ADCON0, GO		    ; EMPIEZA LA CONVERSIÓN
-CHECK_AD:
-    BTFSC   ADCON0, GO			; revisa que terminó la conversión
-    GOTO    $-1
-    BCF	    PIR1, ADIF			; borramos la bandera del adc
-    MOVF    ADRESH, W
-    MOVWF   PORTB			; mueve adresh al puerto b
     
-CHECK_RCIF:			    ; RECIBE EN RX y lo muestra en PORTD
+CHECK_RCIF:			    ; RECIBE EN RX y lo muestra en PORTA
+    
     BTFSS   PIR1, RCIF
     GOTO    CHECK_TXIF
     MOVF    RCREG, W
-    MOVWF   PORTD
-    
-CHECK_TXIF: 
-    MOVFW   PORTB		    ; ENVÍA PORTB POR EL TX
-    MOVWF   TXREG
-   
-    BTFSS   PIR1, TXIF
-    GOTO    $-1
+    MOVWF   PORTA
     
     GOTO LOOP
-;*******************************************************************************
+;***************************
     CONFIG_RELOJ
     BANKSEL TRISA
     
     BSF OSCCON, IRCF2
     BCF OSCCON, IRCF1
     BCF OSCCON, IRCF0		    ; FRECUECNIA DE 1MHz
-;    
-;    BCF OSCCON, OSTS		    ; UTILIZAREMOS RELOJ INTERNO
-;    BSF OSCCON, HTS		    ; ESTABLE
-;    BSF OSCCON, SCS		    ; SELECCIONAMOS RELOJ INTERNO COMO EL RELOJ DEL SISTEMA
+
     RETURN
  
  ;--------------------------------------------------------
-    CONFIG_TX_RX
+CONFIG_TX_RX
     BANKSEL TXSTA
     BCF	    TXSTA, SYNC		    ; ASINCRÓNO
     BSF	    TXSTA, BRGH		    ; LOW SPEED
@@ -125,29 +103,6 @@ CONFIG_IO
     CLRF    PORTD
     CLRF    PORTE
     RETURN    
-;-----------------------------------------------
-    CONFIG_ADC
-    BANKSEL PORTA
-    BCF ADCON0, ADCS1
-    BSF ADCON0, ADCS0		; FOSC/8 RELOJ TAD
-    
-    BCF ADCON0, CHS3		; CH0
-    BCF ADCON0, CHS2
-    BCF ADCON0, CHS1
-    BCF ADCON0, CHS0	
-    BANKSEL TRISA
-    BCF ADCON1, ADFM		; JUSTIFICACIÓN A LA IZQUIERDA
-    BCF ADCON1, VCFG1		; VSS COMO REFERENCIA VREF-
-    BCF ADCON1, VCFG0		; VDD COMO REFERENCIA VREF+
-    BANKSEL PORTA
-    BSF ADCON0, ADON		; ENCIENDO EL MÓDULO ADC
-    
-    BANKSEL TRISA
-    BSF	    TRISA, RA0		; RA0 COMO ENTRADA
-    BANKSEL ANSEL
-    BSF	    ANSEL, 0		; ANS0 COMO ENTRADA ANALÓGICA
-    
-    RETURN
 ;-----------------------------------------------
 DELAY_50MS
     MOVLW   .100		    ; 1US 
