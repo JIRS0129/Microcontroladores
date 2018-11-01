80a88baac8ddc513211af2dd03d48f9f3a0e12af
JIRS0129 <jirs0129@gmail.com>
2018-11-01 12:48:22 -0600

    Overall GUI and serial working (not tested with PIC)

---
+++ MiniProyecto/ActualPorgrams/PythonSerialRefresh.py
@@ -0,0 +1,91 @@
+#Python V3.5.2
+
+#Use arduino's TestingSketch for testing purpose
+
+import serial
+import tkinter as tk
+import time
+
+while(1):
+    try:
+        port = "com" + str(int(input(">  COM: ")))
+        break
+    except:
+        print ("Enter a numeric value")
+
+data = serial.Serial(port, 9600)
+
+def refresh(x):
+    result = None
+    data.reset_input_buffer()
+    time.sleep(.5)
+    while(result is None):
+        result = data.readline()
+    print(type(result))
+    return(result)
+
+class Mainframe(tk.Frame):
+    
+    def __init__(self,master,*args,**kwargs):
+        tk.Frame.__init__(self,master,*args,**kwargs)
+
+        tk.Label(self, text = "Angulo", font=("Courier", 15)).place(x = 40, y = 30)
+        
+        self.spBox = tk.Spinbox(self, from_=0, to=180, font=("Courier", 13))
+        self.spBox.place(x = 20, y = 70, width = 120)
+        
+        self.Temperature = tk.IntVar()
+        tk.Label(self,textvariable = self.Temperature, font=("Courier", 15)).place(x = 180, y = 30)
+        self.TimerInterval = 500
+
+        tk.Button(self, text = "Enviar", command = self.send, font=("Courier", 15)).place(x = 180, y = 65)
+        
+        self.Temp = 0
+        
+        self.GetTemp()
+        
+    def GetTemp(self):
+        self.Temperature.set(self.Temp)
+        self.Temp += 1
+        
+        # Now repeat call
+        self.after(self.TimerInterval,self.GetTemp)
+
+    def send(self):
+        data.write(bytes(self.spBox.get().encode()))
+        data.write(b'\n')
+
+class leftFrame(tk.Frame):
+    
+    def __init__(self,master,*args,**kwargs):
+        tk.Frame.__init__(self,master,*args,**kwargs)
+
+        tk.Label(self, text = "Voltaje", font=("Courier", 15)).place(x = 20, y = 30)
+        
+        self.Voltaje = tk.IntVar()
+        tk.Label(self,textvariable = self.Voltaje, font=("Courier", 13)).place(x = 55, y = 70)
+
+        self.TimerInterval = 500
+
+        self.Volt = 1
+
+        self.refreshVoltaje()
+
+    def refreshVoltaje(self):
+        self.Volt = refresh(self.Volt)
+        self.Voltaje.set(self.Volt)
+
+        self.after(self.TimerInterval,self.refreshVoltaje)
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+               
+        self.title('Miniproyecto de Microcontroladores')
+        self.geometry('450x200')
+
+        leftFrame(self).place(x = 20, y = 30, width=150, height=300)
+        Mainframe(self).place(x = 160, y = 30, width=300, height=300)
+        self.mainloop()
+    
+App()
---
+++ MiniProyecto/TestingStuff/TestingSketch/TestingSketch.ino
@@ -0,0 +1,101 @@
+/*
+  Serial Event example
+
+ When new serial data arrives, this sketch adds it to a String.
+ When a newline is received, the loop prints the string and
+ clears it.
+
+ A good test for this is to try it with a GPS receiver
+ that sends out NMEA 0183 sentences.
+
+ Created 9 May 2011
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/SerialEvent
+
+ */
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+byte menu = 0;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    if(inputString == "Command1"){
+      Serial.println("Received");
+      digitalWrite(13, HIGH);
+    }else if(inputString == "Command2"){
+      Serial.println("Okay");
+      digitalWrite(13, LOW);
+    }else if(inputString == "Command3"){
+      Serial.println("What?");
+      digitalWrite(13, LOW);
+      delay(1000);
+      digitalWrite(13, HIGH);
+      delay(1000);
+      digitalWrite(13, LOW);
+      delay(1000);
+      digitalWrite(13, HIGH);
+      delay(1000);
+      digitalWrite(13, LOW);
+    }else if(inputString == "3"){
+      digitalWrite(13, HIGH);
+    }
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }else{
+    switch(menu){
+      case 1:
+        Serial.println("3.0");
+        break;
+      case 2:
+        Serial.println("2.0");
+        break;
+      case 3:
+        Serial.println("1.0");
+        break;
+    }
+    delay(1000);
+    menu++;
+    if(menu == 4){
+      menu = 0;
+    }
+  }
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }else{
+      inputString += inChar;
+    }
+  }
+}
+
+
